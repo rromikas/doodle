b9a36af7b56e9dd85fbdcbb591799320f1185b3e
dmkrap <ProgramingDomiz69@users.noreply.github.com>
2021-10-04 20:18:04 +0300

    Added player to SingnalR hub

--- server/Hubs/GameHub.cs
+++ server/Hubs/GameHub.cs
@@ -1,13 +1,37 @@
+﻿using GameServer.Constants;
+using GameServer.Models;
 using Microsoft.AspNetCore.SignalR;
+using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace GameServer.Hubs
 {
     public class GameHub : Hub
     {
-        public async Task SendMessage(string user, string message)
+        private readonly Dictionary<string, Player> _players = new Dictionary<string, Player>();
+        private readonly Timer _gameClock;
+
+        public GameHub()
         {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            _gameClock = new Timer(GameTickCallBack, null, 1000, 250);
+        }
+
+        public async Task Move(string playerId, Coordinate cooradinate)
+        {
+            _players[playerId].Coordinate = cooradinate;
+            await Clients.All.SendAsync(HubMethods.ALL_PLAYERS_INFO, _players);
+        }
+
+        public async Task Login(string playerId)
+        {
+            _players.Add(playerId, new Player(playerId));
+            await Clients.All.SendAsync(HubMethods.ALL_PLAYERS_INFO, _players);
+        }
+
+        private async void GameTickCallBack(object sate)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", "Just a message");
         }
     }
 }
