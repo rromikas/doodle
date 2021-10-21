1f0339d01764bed098b38211a5a85b78fd0ba62f
JustasGl <35509569+JustasGl@users.noreply.github.com>
2021-10-21 19:40:17 +0300

    Prototype

--- server/Models/BaseFood.cs
+++ server/Models/BaseFood.cs
@@ -3,12 +3,18 @@ using GameServer.Constants;
 
 namespace GameServer.Models
 {
-    public class BaseFood: BaseUnit
+    public class BaseFood : BaseUnit, ICloneable
     {
         private int _pointReward;
+
         public BaseFood(Coordinate coordinate, int pointReward, ColorTypes color) : base(coordinate, color)
         {
             _pointReward = pointReward;
+        }
+
+        public object Clone()
+        {
+            return new BaseFood(this.Coordinate, _pointReward, this.Color);
         }
     }
 }
